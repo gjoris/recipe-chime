110e6af5abfbfa1df612413e8a2ca70921c60dc1
Geroen Joris <gerjoris@amazon.com>
2020-07-30 15:48:57 +0200

    Install script for Mac

---
+++ install_mac.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+mkdir -p ~/Library/Application\ Support/Franz/recipes/dev/chime
+cp icon* ~/Library/Application\ Support/Franz/recipes/dev/chime/
+cp *.js ~/Library/Application\ Support/Franz/recipes/dev/chime/
+cp package.json ~/Library/Application\ Support/Franz/recipes/dev/chime/
