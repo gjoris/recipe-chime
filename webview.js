b4c1ebae15fab21dc070356fb6c3e225288af3e5
Geroen Joris <gerjoris@amazon.com>
2020-07-30 15:48:39 +0200

    Version 1.1.0 - Support for unread messages

--- webview.js
+++ webview.js
@@ -1,3 +1,20 @@
 module.exports = (Franz) => {
-    // TODO Find a way to get the number of messages from Amazon Chime
+
+    const getMessages = function() {
+        let totalUnreadMessages = 0;
+
+        const allPossibleUnreadBadges = document.querySelectorAll(".UnreadBadge");
+
+        allPossibleUnreadBadges.forEach(((value, key, parent) => {
+            //Skip the "Call history"
+            if (!value.parentElement?.href?.endsWith("/history")) {
+                totalUnreadMessages += parseInt(value.innerHTML, 10);
+            }
+        }))
+
+        if (totalUnreadMessages > 0) Franz.setBadge(totalUnreadMessages);
+    }
+
+    Franz.loop(getMessages);
+
 };
